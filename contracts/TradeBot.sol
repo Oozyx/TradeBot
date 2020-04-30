01d236cc6c9f8945af23f229200d85d0008fef4d
oozyx <hendrik.oosenbrug@gmail.com>
2020-04-30 14:43:19 -0400

    Changed contract to use Uniswap V2 and removed Kyber implementation for now.

--- contracts/TradeBot.sol
+++ contracts/TradeBot.sol
@@ -1,96 +1,102 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.6.0;
 
-contract ERC20 {
-    function totalSupply() public view returns (uint supply);
-    function balanceOf(address _owner) public view returns (uint balance);
-    function transfer(address _to, uint _value) public returns (bool success);
-    function transferFrom(address _from, address _to, uint _value) public returns (bool success);
-    function approve(address _spender, uint _value) public returns (bool success);
-    function allowance(address _owner, address _spender) public view returns (uint remaining);
-    function decimals() public view returns(uint digits);
-    event Approval(address indexed _owner, address indexed _spender, uint _value);
-}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract UniswapFactoryInterface {
-    // Public Variables
-    address public exchangeTemplate;
-    uint256 public tokenCount;
-    // Create Exchange
-    function createExchange(address token) external returns (address exchange);
-    // Get Exchange and Token Info
-    function getExchange(address token) external view returns (address exchange);
-    function getToken(address exchange) external view returns (address token);
-    function getTokenWithId(uint256 tokenId) external view returns (address token);
-    // Never use
-    function initializeFactory(address template) external;
-}
+interface IUniswapV2Router01 {
+  function WETH() external view returns (address);
 
-contract UniswapExchangeInterface {
-    // Address of ERC20 token sold on this exchange
-    function tokenAddress() external view returns (address token);
-    // Address of Uniswap Factory
-    function factoryAddress() external view returns (address factory);
-    // Provide Liquidity
-    function addLiquidity(uint256 min_liquidity, uint256 max_tokens, uint256 deadline) external payable returns (uint256);
-    function removeLiquidity(uint256 amount, uint256 min_eth, uint256 min_tokens, uint256 deadline) external returns (uint256, uint256);
-    // Get Prices
-    function getEthToTokenInputPrice(uint256 eth_sold) external view returns (uint256 tokens_bought);
-    function getEthToTokenOutputPrice(uint256 tokens_bought) external view returns (uint256 eth_sold);
-    function getTokenToEthInputPrice(uint256 tokens_sold) external view returns (uint256 eth_bought);
-    function getTokenToEthOutputPrice(uint256 eth_bought) external view returns (uint256 tokens_sold);
-    // Trade ETH to ERC20
-    function ethToTokenSwapInput(uint256 min_tokens, uint256 deadline) external payable returns (uint256  tokens_bought);
-    function ethToTokenTransferInput(uint256 min_tokens, uint256 deadline, address recipient) external payable returns (uint256  tokens_bought);
-    function ethToTokenSwapOutput(uint256 tokens_bought, uint256 deadline) external payable returns (uint256  eth_sold);
-    function ethToTokenTransferOutput(uint256 tokens_bought, uint256 deadline, address recipient) external payable returns (uint256  eth_sold);
-    // Trade ERC20 to ETH
-    function tokenToEthSwapInput(uint256 tokens_sold, uint256 min_eth, uint256 deadline) external returns (uint256  eth_bought);
-    function tokenToEthTransferInput(uint256 tokens_sold, uint256 min_eth, uint256 deadline, address recipient) external returns (uint256  eth_bought);
-    function tokenToEthSwapOutput(uint256 eth_bought, uint256 max_tokens, uint256 deadline) external returns (uint256  tokens_sold);
-    function tokenToEthTransferOutput(uint256 eth_bought, uint256 max_tokens, uint256 deadline, address recipient) external returns (uint256  tokens_sold);
-    // Trade ERC20 to ERC20
-    function tokenToTokenSwapInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address token_addr) external returns (uint256  tokens_bought);
-    function tokenToTokenTransferInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address recipient, address token_addr) external returns (uint256  tokens_bought);
-    function tokenToTokenSwapOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address token_addr) external returns (uint256  tokens_sold);
-    function tokenToTokenTransferOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address recipient, address token_addr) external returns (uint256  tokens_sold);
-    // Trade ERC20 to Custom Pool
-    function tokenToExchangeSwapInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address exchange_addr) external returns (uint256  tokens_bought);
-    function tokenToExchangeTransferInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address recipient, address exchange_addr) external returns (uint256  tokens_bought);
-    function tokenToExchangeSwapOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address exchange_addr) external returns (uint256  tokens_sold);
-    function tokenToExchangeTransferOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address recipient, address exchange_addr) external returns (uint256  tokens_sold);
-    // ERC20 comaptibility for liquidity tokens
-    bytes32 public name;
-    bytes32 public symbol;
-    uint256 public decimals;
-    function transfer(address _to, uint256 _value) external returns (bool);
-    function transferFrom(address _from, address _to, uint256 value) external returns (bool);
-    function approve(address _spender, uint256 _value) external returns (bool);
-    function allowance(address _owner, address _spender) external view returns (uint256);
-    function balanceOf(address _owner) external view returns (uint256);
-    function totalSupply() external view returns (uint256);
-    // Never use
-    function setup(address token_addr) external;
-}
-
-contract KyberNetworkProxyInterface {
-    function maxGasPrice() public view returns(uint);
-    function getUserCapInWei(address user) public view returns(uint);
-    function getUserCapInTokenWei(address user, ERC20 token) public view returns(uint);
-    function enabled() public view returns(bool);
-    function info(bytes32 id) public view returns(uint);
-    function getExpectedRate(ERC20 src, ERC20 dest, uint srcQty) public view returns (uint expectedRate, uint slippageRate);
-    function tradeWithHint(ERC20 src, uint srcAmount, ERC20 dest, address destAddress, uint maxDestAmount, uint minConversionRate, address walletId, bytes memory hint) public payable returns(uint);
+  function addLiquidity(
+    address tokenA,
+    address tokenB,
+    uint amountADesired,
+    uint amountBDesired,
+    uint amountAMin,
+    uint amountBMin,
+    address to,
+    uint deadline
+  ) external returns (uint amountA, uint amountB, uint liquidity);
+  function addLiquidityETH(
+    address token,
+    uint amountTokenDesired,
+    uint amountTokenMin,
+    uint amountETHMin,
+    address to,
+    uint deadline
+  ) external payable returns (uint amountToken, uint amountETH, uint liquidity);
+  function removeLiquidity(
+    address tokenA,
+    address tokenB,
+    uint liquidity,
+    uint amountAMin,
+    uint amountBMin,
+    address to,
+    uint deadline
+  ) external returns (uint amountA, uint amountB);
+  function removeLiquidityETH(
+    address token,
+    uint liquidity,
+    uint amountTokenMin,
+    uint amountETHMin,
+    address to,
+    uint deadline
+  ) external returns (uint amountToken, uint amountETH);
+  function removeLiquidityWithPermit(
+    address tokenA,
+    address tokenB,
+    uint liquidity,
+    uint amountAMin,
+    uint amountBMin,
+    address to,
+    uint deadline,
+    bool approveMax, uint8 v, bytes32 r, bytes32 s
+  ) external returns (uint amountA, uint amountB);
+  function removeLiquidityETHWithPermit(
+    address token,
+    uint liquidity,
+    uint amountTokenMin,
+    uint amountETHMin,
+    address to,
+    uint deadline,
+    bool approveMax, uint8 v, bytes32 r, bytes32 s
+  ) external returns (uint amountToken, uint amountETH);
+  function swapExactTokensForTokens(
+    uint amountIn,
+    uint amountOutMin,
+    address[] calldata path,
+    address to,
+    uint deadline
+  ) external returns (uint[] memory amounts);
+  function swapTokensForExactTokens(
+    uint amountOut,
+    uint amountInMax,
+    address[] calldata path,
+    address to,
+    uint deadline
+  ) external returns (uint[] memory amounts);
+  function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
+    external
+    payable
+    returns (uint[] memory amounts);
+  function swapTokensForExactETH(uint amountOut, uint amountInMax, address[] calldata path, address to, uint deadline)
+    external
+    returns (uint[] memory amounts);
+  function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
+    external
+    returns (uint[] memory amounts);
+  function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline)
+    external
+    payable
+    returns (uint[] memory amounts);
 }
 
 contract TradeBot {
-  address internal constant UNISWAP_FACTORY_ADDRESS = 0xf5D915570BC477f9B8D6C0E980aA81757A3AaC36; // Rinkeby
-  address internal constant KYBER_PROXY_ADDRESS     = 0xF77eC7Ed5f5B9a5aee4cfa6FFCaC6A4C315BaC76; // Rinkeby
-  ERC20   constant internal KYBER_ETH_TOKEN_ADDRESS = ERC20(0x00eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee);
-  uint    constant internal MAX_QTY                 = (10**28);
-  bytes   constant PERM_HINT                        = "";
-  address owner;
-  UniswapFactoryInterface uniswapFactory;
-  KyberNetworkProxyInterface kyberProxy;
+  address internal constant UNISWAP_ROUTER_ADDRESS = 0xcDbE04934d89e97a24BCc07c3562DC8CF17d8167; // Rinkeby
+  uint    internal constant TOKEN_18_DECIMALS      = (10**18);
+  address                   owner;
+  IUniswapV2Router01        uniswapRouter;
+
+  event Log(string log);
+  event UniswapTrade(uint inputAmount, uint outputAmount);
 
   modifier onlyOwner() {
     require (
@@ -102,80 +108,62 @@ contract TradeBot {
 
   constructor() public {
     owner = msg.sender;
-    uniswapFactory = UniswapFactoryInterface(UNISWAP_FACTORY_ADDRESS);
-    kyberProxy = KyberNetworkProxyInterface(KYBER_PROXY_ADDRESS);
-  }
-
-  function approveTokenTrade(address tokenAddress) external onlyOwner {
-    ERC20 token = ERC20(tokenAddress);
-    token.approve(address(this), token.balanceOf(msg.sender));
+    uniswapRouter = IUniswapV2Router01(UNISWAP_ROUTER_ADDRESS);
   }
 
   /*
     Uniswap methods
    */
-  function swapEthForTokenWithUniswap(address tokenAddress) public onlyOwner payable {
-    UniswapExchangeInterface exchange = UniswapExchangeInterface(uniswapFactory.getExchange(tokenAddress));
-    uint tokenAmount = exchange.getEthToTokenInputPrice(msg.value);
-    exchange.ethToTokenSwapInput.value(msg.value)(
-      tokenAmount,
-      now
-    );
+  function swapEthForTokenWithUniswap(uint ethAmount, address tokenAddress) public onlyOwner {
+    // Verify we have enough funds
+    require(ethAmount <= address(this).balance, "Not enought Eth in contract to perform swap.");
+
+    // Build arguments for uniswap router call
+    address[] memory path = new address[](2);
+    path[0] = uniswapRouter.WETH();
+    path[1] = tokenAddress;
+
+    // Make the call and log the results
+    uint[] memory result = uniswapRouter.swapExactETHForTokens.value(ethAmount)(1, path, address(this), now);
+    emit Log("Uniswap Eth for token swap complete.");
+    emit UniswapTrade(result[0], result[1]);
   }
 
-  function swapTokenForEtherWithUniswap(address tokenAddress) public onlyOwner {
-    UniswapExchangeInterface exchange = UniswapExchangeInterface(uniswapFactory.getExchange(tokenAddress));
+  function swapTokenForEtherWithUniswap(uint tokenAmount, address tokenAddress) public onlyOwner {
+    // Verify we have enough funds
     ERC20 token = ERC20(tokenAddress);
-    uint tokenAmount = token.balanceOf(msg.sender);
-    token.transferFrom(msg.sender, address(this), tokenAmount);
-    uint ethAmount = exchange.getTokenToEthInputPrice(tokenAmount);
-    exchange.tokenToEthSwapInput(
-      tokenAmount,
-      ethAmount,
-      now
-      );
+    require(tokenAmount * TOKEN_18_DECIMALS <= token.balanceOf(address(this)), "Not enough tokens in contract to perform swap.");
+
+    // Build arguments for uniswap router call
+    address[] memory path = new address[](2);
+    path[0] = tokenAddress;
+    path[1] = uniswapRouter.WETH();
+
+    // Make the call and log the results
+    uint[] memory result = uniswapRouter.swapExactTokensForETH(tokenAmount * TOKEN_18_DECIMALS, 1, path, address(this), now);
+    emit Log("Uniswap token for Eth swap complete.");
+    emit UniswapTrade(result[0], result[1]);
   }
 
   /*
-    Kyber methods
+    Deposit and Withdrawal methods
    */
-  // Returns (expected rate, slippage rate)
-  function getConversionRatesKyber(ERC20 srcToken, ERC20 destToken, uint srcQty) public view returns (uint, uint) {
-    return kyberProxy.getExpectedRate(srcToken, destToken, srcQty);
+  function depositEth() external payable {
+    // Nothing to do
   }
 
-  function swapEthForTokenWithKyber(ERC20 destToken) public onlyOwner payable {
-    uint minConversionRate;
-
-    // Get minimum conversion rate
-    (minConversionRate, ) = getConversionRatesKyber(KYBER_ETH_TOKEN_ADDRESS, destToken, msg.value);
-
-    // Make the swap to the destination address
-    //tradeWithHint(ERC20 src, uint srcAmount, ERC20 dest, address destAddress, uint maxDestAmount, uint minConversionRate, address walletId, bytes memory hint)
-    kyberProxy.tradeWithHint(
-      KYBER_ETH_TOKEN_ADDRESS,
-      msg.value,
-      destToken,
-      address(this),
-      MAX_QTY,
-      minConversionRate,
-      address(0),
-      PERM_HINT
-    );
+  function depositToken(address tokenAddress, uint tokenAmount) external {
+    ERC20 token = ERC20(tokenAddress);
+    require (tokenAmount * TOKEN_18_DECIMALS <= token.balanceOf(msg.sender), "Deposit amount exceeds holding amount.");
+    token.transferFrom(msg.sender, address(this), tokenAmount * TOKEN_18_DECIMALS);
   }
 
-  /*
-    Withdrawal methods
-   */
   function withdrawEth() external onlyOwner {
-    //(bool success, ) = msg.sender.call.value(address(this).balance)("");
     msg.sender.transfer(address(this).balance);
-    //require(success, "Transfer failed.");
   }
 
   function withdrawToken(address tokenAddress) external onlyOwner {
     ERC20 token = ERC20(tokenAddress);
-    uint tokenBalance = token.balanceOf(address(this));
-    token.transfer(msg.sender, tokenBalance);
+    token.transfer(msg.sender, token.balanceOf(address(this)));
   }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -72,11 +72,11 @@ module.exports = {
     //   provider: () => new HDWalletProvider(mnemonic, infuraMainnetEndpoint),
     // },
 
-    rinkeby: {
-      network_id: 4,
-      gas: 4500000,
-      gasPrice: 10000000000
-    }
+    // rinkeby: {
+    //   network_id: 4,
+    //   gas: 4500000,
+    //   gasPrice: 10000000000
+    // }
     
     // Useful for private networks
     // private: {
@@ -94,7 +94,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.6.6",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
